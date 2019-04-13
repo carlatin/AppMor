09577543ad392e7c92c5826ee333712c13bf835d
carlatin <robinsonemanuelopez@gmail.com>
2019-04-13 08:53:49 -0600

    aplicacion de sistemas

--- www/js/app.js
+++ www/js/app.js
@@ -50,6 +50,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       templateUrl:'templates/registro.html'
     })
   // if none of the above states are matched, use this as the fallback  $urlRouterProvider.otherwise('/login');
-  $urlRouterProvider.otherwise('/registro');
+  $urlRouterProvider.otherwise('/login');
 
 });
