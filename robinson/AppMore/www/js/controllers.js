17647a1bb412e6a21fe7f2de2036d7ecb4fc15cd
carlatin <robinsonemanuelopez@gmail.com>
2019-04-13 08:19:23 -0600

    pagina de sistemas

---
+++ robinson/AppMore/www/js/controllers.js
@@ -0,0 +1,17 @@
+var config = {
+    apiKey: "AIzaSyCoyGhEUCupffG_4VNptbFW6xJHi49WZpg",
+    authDomain: "appmore-98f53.firebaseapp.com",
+    databaseURL: "https://appmore-98f53.firebaseio.com",
+    projectId: "appmore-98f53",
+    storageBucket: "appmore-98f53.appspot.com",
+    messagingSenderId: "1064596361010"
+  };
+  firebase.initializeApp(config);
+
+angular.module('starter.controllers', [])
+.controller("registro",function($scope){
+
+})
+.controller("registroLogin",function($scope){
+
+});
